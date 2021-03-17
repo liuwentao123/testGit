b7f97dc1de9ad0c18a4cba22aa8891744d046cd7
liuwentao <liu.wentao@immomo.com>
2021-03-17 20:22:37 +0800

    1

--- src/main/java/com/lwt/testgit/Test.java
+++ src/main/java/com/lwt/testgit/Test.java
@@ -6,16 +6,6 @@ package com.lwt.testgit;
 
 public class Test {
     public void test(){
-        System.out.println("liu");
-        System.out.println("wen");
-        System.out.println("tao");
-        System.out.println("你");
-        System.out.println("好");
-        System.out.println("呀");
-        System.out.println("!");
-        System.out.println("!");
-        System.out.println("!");
-        System.out.println("!");
-        System.out.println("!");
+        System.out.println("1");
     }
 }
